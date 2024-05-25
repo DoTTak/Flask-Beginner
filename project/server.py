7884ddf26bc5c8094a78afa014e16a14795c44d3
DoTTak <kdw8220@gmail.com>
2024-05-25 15:27:27 +0900

    Update: server.py - add routing

--- project/server.py
+++ project/server.py
@@ -5,10 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return 'Hello World'
+
+@app.route('/random')
+def rand(): # 함수 명 random 지정 시 random 모듈과 동일한 이름을 가지므로 에러가 발생
     random_color = "#%06x" % random.randint(0, 0xFFFFFF)
     return "Random: " + \
         "<strong style='color: "+random_color+";'>" + \
             str(random.random()) + \
         "</strong>"
+
+@app.route('/hello')
+def hello():
+    return 'Hello Flask'
 
 app.run(port=8000, debug=True)
