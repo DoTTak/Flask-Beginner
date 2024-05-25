30cb798d1c451f932abc469bba2473f98509bbaa
DoTTak <kdw8220@gmail.com>
2024-05-25 17:43:50 +0900

    Update: server.py - add index, read, create)

--- project/server.py
+++ project/server.py
@@ -3,24 +3,21 @@ import random
 
 app = Flask(__name__)
 
+list_items = [] # 아이템을 저장하기 위한 목록
+
 @app.route('/')
 def index():
-    return 'Hello World'
+    # 목록이 조회되는 화면
+    return "index"
 
-@app.route('/random')
-def rand(): # 함수 명 random 지정 시 random 모듈과 동일한 이름을 가지므로 에러가 발생
-    random_color = "#%06x" % random.randint(0, 0xFFFFFF)
-    return "Random: " + \
-        "<strong style='color: "+random_color+";'>" + \
-            str(random.random()) + \
-        "</strong>"
+@app.route('/read/<int:id>')
+def read(id):
+    # 아이템을 조회되는 화면
+    return "read: " + str(id)
 
-@app.route('/hello')
-def hello():
-    return 'Hello Flask'
-
-@app.route('/read/<int:id>') 
-def read(id): # 라우팅 경로에 id 위치의 값을 받기 위해 동일한 이름의 id 인자값을 설정
-    return 'Read: ' + str(id) # int형 인자 이므로, 문자열 형변환을 수행
+@app.route('/create')
+def create():
+    # 아이템을 생성하는 화면
+    return "create"
 
 app.run(port=8000, debug=True)
