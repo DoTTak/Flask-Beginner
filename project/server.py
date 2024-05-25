adc2f6e64f2d6d69356c91c3e32b5ab9ff0ec9ef
DoTTak <kdw8220@gmail.com>
2024-05-25 19:32:39 +0900

    Update: 목록 조회, 아이템 읽기, 아이템 생성 처리 로직 구현

--- project/server.py
+++ project/server.py
@@ -1,23 +1,81 @@
 from flask import Flask
+from flask import request
 import random
 
 app = Flask(__name__)
 
-list_items = [] # 아이템을 저장하기 위한 목록
+list_items = [ # 아이템을 저장하기 위한 목록
+    {"id": 1, "title": "html", "content": "html is ..."},
+    {"id": 2, "title": "javascript", "content": "javascript is ..."},
+    {"id": 3, "title": "css", "content": "css is ..."}
+]
+
+def template(contents, content):
+    # 응답할 html 코드
+    html = f'''
+    <html>
+    <body>
+        <h1>List</h1>
+        <ol>
+            {contents}
+        </ol>
+        {content}
+    </body>
+    </html>
+    '''
+
+    return html
+
+def getItems():
+    # html 응답 코드 내 목록을 조회하기 위한 구현부
+    li_tags = ''
+    for item in list_items:
+        li_tags += f'<li><a href="/read/{item["id"]}">{item["title"]}</a></li>'
+    
+    return li_tags
 
 @app.route('/')
 def index():
     # 목록이 조회되는 화면
-    return "index"
+    return template(getItems(), "")
 
 @app.route('/read/<int:id>')
 def read(id):
     # 아이템을 조회되는 화면
-    return "read: " + str(id)
+    
+    # 선택한 아이템에 대한 제목과 콘텐츠를 담기 위한 구현부
+    title = '' # 제목을 저장할 변수
+    content = '' # 콘텐츠를 저장할 변수
+    for item in list_items:
+        # 목록을 순회할 때 선택한 아이템의 경우 제목과 콘텐츠를 변수에 저장
+        if id == item['id']:
+            title = item["title"]
+            content = item["content"]
+            break
+    
+    return template(getItems(), f"""<hr><h2>Title: {title}</h2>{content}""")
 
-@app.route('/create')
+@app.route('/create', methods=['GET', 'POST'])
 def create():
     # 아이템을 생성하는 화면
-    return "create"
+
+    # GET 요청을 수행하는 로직
+    if request.method == 'GET':
+
+        # 아이템 생성을 위한 html 코드
+        html = """
+        <form action="/create" method="POST">
+            <p>title: <input type="text" name="title" placeholder="title"></input></p>
+            <p><textarea name="content" placeholder="content"></textarea></p>
+            <input type="submit" value="create">
+        </form>
+        """
+        return template(getItems(), f"<hr>{html}")
+    
+    # POST 요청을 수행하는 로직
+    else:
+        title = request.form['title'] # 클라이언트로 부터 HTTP Request 데이터 'title' 을 받음
+        content = request.form['content'] # 클라이언트로 부터 HTTP Request 데이터 'content' 을 받음
+        return title + ", " + content
 
 app.run(port=8000, debug=True)
