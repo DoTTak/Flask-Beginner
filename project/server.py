1f7757b150389b3249741c47f24d3c6e9b846417
DoTTak <kdw8220@gmail.com>
2024-05-25 14:18:26 +0900

    Update: server.py - return random string

--- project/server.py
+++ project/server.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Flask'
+    return str(random.random())
 
 app.run(port=8000, debug=True)
