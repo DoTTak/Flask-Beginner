b390bbc961d52543914e8d835560c083bce23dd9
DoTTak <kdw8220@gmail.com>
2024-05-25 15:45:30 +0900

    Update: server.py - add routing with variable(+ converter)

--- project/server.py
+++ project/server.py
@@ -19,4 +19,8 @@ def rand(): # 함수 명 random 지정 시 random 모듈과 동일한 이름을 
 def hello():
     return 'Hello Flask'
 
+@app.route('/read/<int:id>') 
+def read(id): # 라우팅 경로에 id 위치의 값을 받기 위해 동일한 이름의 id 인자값을 설정
+    return 'Read: ' + str(id) # int형 인자 이므로, 문자열 형변환을 수행
+
 app.run(port=8000, debug=True)
